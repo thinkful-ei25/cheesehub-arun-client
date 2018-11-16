fd6a6f8c4ac2114110db35d4aeb0b9cb22bbca2c
Arun Seehra <seehra.a@gmail.com>
2018-11-16 14:24:58 -0600

    Fix config variables
    It turns out that create react app will only pass through REACT_APP_* config
    variables:
    
    > Note: You must create custom environment variables beginning with REACT_APP_.
    > Any other variables except NODE_ENV will be ignored to avoid accidentally
    > exposing a private key on the machine that could have the same name. Changing
    > any environment variables will require you to restart the development server
    > if it is running.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
+// prettier-ignore
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+  || 'http://localhost:8080';
