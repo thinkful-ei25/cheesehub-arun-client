c3bb18688d4759dab25ea8dec4b1d466904086f9
Arun Seehra <seehra.a@gmail.com>
2018-11-16 11:31:57 -0600

    Add fetch cheese synchronous and async actions

---
+++ src/actions/cheeses.js
@@ -0,0 +1,42 @@
+import { API_BASE_URL } from '../config';
+import {
+  FETCH_CHEESES_ERROR,
+  FETCH_CHEESES_SUCCESS,
+  FETCH_CHEESES_REQUEST,
+} from './types';
+
+const fetchCheesesRequest = () => ({
+  type: FETCH_CHEESES_REQUEST,
+});
+
+const fetchCheesesSuccess = cheeses => ({
+  type: FETCH_CHEESES_SUCCESS,
+  cheeses,
+});
+
+const fetchCheesesError = error => ({
+  type: FETCH_CHEESES_ERROR,
+  error,
+});
+
+// eslint-disable-next-line import/prefer-default-export
+export const fetchCheeses = () => (dispatch) => {
+  dispatch(fetchCheesesRequest());
+  return fetch(API_BASE_URL)
+    .then((res) => {
+      if (!res.ok) {
+        const contentType = res.headers.get('content-type');
+        if (contentType && contentType.startsWith('application/json')) {
+          return res.json().then(err => Promise.reject(err));
+        }
+
+        const error = new Error(res.statusText);
+        error.code = res.status;
+        return Promise.reject(error);
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .then(cheeses => dispatch(fetchCheesesSuccess(cheeses)))
+    .catch(error => dispatch(fetchCheesesError(error)));
+};
---
+++ src/config.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+
+export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
